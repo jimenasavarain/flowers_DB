2d93f2cecfd5dc5b6278775b22e3bfbc10a5a283
karizmo <32525686+karizmo@users.noreply.github.com>
2019-02-20 14:02:04 -0800

    added 2 headers

--- postFlower.php
+++ postFlower.php
@@ -1,5 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 try {
   $conn = new PDO("mysql:host=pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=vxhl9pgg2au3blht","djjgopngh67f0sdi","o2hle778718kdubu");
 } catch (PDOException $e){
