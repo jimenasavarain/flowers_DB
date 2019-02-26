d7e4c97b4668a430ba6cf2337e794672eba2a270
karizmo <32525686+karizmo@users.noreply.github.com>
2019-02-25 21:33:26 -0800

    updated selectflower

--- selectFlower.php
+++ selectFlower.php
@@ -1,22 +1,22 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 try {
-  $conn = new PDO("pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","djjgopngh67f0sdi","o2hle778718kdubu");
+  $conn = new PDO("mysql:host=pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=vxhl9pgg2au3blht","djjgopngh67f0sdi","o2hle778718kdubu");
 } catch (PDOException $e){
   echo "Error".$e->getMessage();
 }
 
-$adminid=1;
+// value of adminid will be 1 for now
+$adminid=$_POST['adminnum'];
 
-//$query = "INSERT INTO flowers (adminid, name, price, description, reviews, dateadded, type, image) VALUES ('$adminid', '$name', '$price', '$description', '$reviews', '$dateadded', '$type', '$img')";
-
-$query = "SELECT * FROM flowers";
+$query = "SELECT * FROM flowers WHERE adminid='$adminid'";
 
 $result = $conn->query($query);
 
 if($result){
   $flowers = $result->fetchAll(PDO::FETCH_CLASS);
-
   echo json_encode ($flowers);
 } else {
     echo json_encode (false);
