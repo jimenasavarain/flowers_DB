3758b1a723f37961548e65937c02523d57472499
karizmo <32525686+karizmo@users.noreply.github.com>
2019-03-16 17:19:01 -0700

    fix adduser

--- addUser.php
+++ addUser.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 try {
-  $conn = new PDO("pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","djjgopngh67f0sdi","o2hle778718kdubu");
+  $conn = new PDO("mysql:host=pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=vxhl9pgg2au3blht","djjgopngh67f0sdi","o2hle778718kdubu");
 } catch (PDOException $e){
   echo "Error".$e->getMessage();
 }
