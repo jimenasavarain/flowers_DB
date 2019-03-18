7555be954aa47def8956c627ae8860be2d6f6891
karizmo <32525686+karizmo@users.noreply.github.com>
2019-03-18 11:38:21 -0700

    added selectuser php

--- selectUser.php
+++ selectUser.php
@@ -1,7 +1,9 @@
 <?php
-header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 try {
-  $conn = new PDO("pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","djjgopngh67f0sdi","o2hle778718kdubu");
+  $conn = new PDO("mysql:host=pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=vxhl9pgg2au3blht","djjgopngh67f0sdi","o2hle778718kdubu");
 } catch (PDOException $e){
   echo "Error".$e->getMessage();
 }
@@ -9,21 +11,40 @@ try {
 $email = $_POST["email"];
 $password = $_POST["password"];
 
-$query = "SELECT email, password FROM admins WHERE email='$email' and password='$password'";
+// $query = "SELECT email, password FROM users WHERE email='$email' and password='$password'";
+
+$query = "SELECT * FROM users WHERE email='$email'";
+        $result = $conn->query($sql);
+        if ($result->num_rows === 1) {
+            $row = $result->fetch_array(MYSQLI_ASSOC);
+            if (password_verify($password, $row['password'])) {
+
+                //Password matches, so create the session
+                // $_SESSION['user'] = $row['user_id'];
+                // header("Location: http://www.example.com/logged_in.php");
+
+                echo json_encode("Match");
+
+            }else{
+                echo json_encode("The username or password do not match");
+            }
+          }else{
+            echo json_encode(false);
+          }
 
 //"SELECT * FROM users WHERE username='$username' AND password = '$password';
 
 //USE THIS TO TEST IF INFO IS GOING INTO DATABASE:
 //$query = "INSERT INTO users (email, password, username, status) VALUES ('test', 'test', 'test', 1)";
 
-$result = $conn->query($query);
-if($result){
-  $users=$result->fetchAll();
-  echo json_encode($admins); 
-  
-} else {
-  echo json_encode(false);
-}
+// $result = $conn->query($query);
+// if($result){
+//   $users=$result->fetchAll();
+//   echo json_encode($users);
+//
+// } else {
+//   echo json_encode(false);
+// }
 $conn=null;
 $result=null;
 ?>
