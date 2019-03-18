8686a3fbde1f6b5d76a967e9dddfe9891d6f0b0d
jimenasavarain <32525730+jimenasavarain@users.noreply.github.com>
2019-03-18 12:52:58 -0700

    reviews php
    to add reviews to each flower item

---
+++ reviews.php
@@ -0,0 +1,26 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+try {
+  $conn = new PDO("mysql:host=pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=vxhl9pgg2au3blht","djjgopngh67f0sdi","o2hle778718kdubu");
+} catch (PDOException $e){
+  echo "Error".$e->getMessage();
+}
+
+$flowerid =$_POST["flowerid"];
+$reviews =$_POST["review"];
+
+$query = "INSERT INTO flowers (reviews) VALUE ('$reviews') WHERE flower_id='$flower_id'";
+
+$result = $conn->query($query);
+
+if($result){
+  echo json_encode(true); 
+  
+} else {
+  echo json_encode(false);
+}
+$conn=null;
+$result=null;
+?>
